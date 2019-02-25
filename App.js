f44543f07974013379b671eebf4275fb78c78d05
FarrahAisyah <47972916+FarrahAisyah@users.noreply.github.com>
2019-02-25 19:21:02 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Farrah, you can do it. </Text>
+        <Text> Siti Nur Farrah Aisyah Binti Zainal Abidin 1612272 </Text>
       </View>
     );
   }
