460b6fb49f25849e85cf73af6133838e12bb3cec
FarrahAisyah <47972916+FarrahAisyah@users.noreply.github.com>
2019-02-25 18:52:04 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Farrah</Text>
+        <Text>Farrah, you can do it. </Text>
       </View>
     );
   }
